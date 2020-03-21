ecae7d14ce356557f933fbf76151f82d47eb6c39
Christoph Fricke <christoph@frickeonline.de>
2020-03-21 19:53:35 +0100

    Fix type errors

--- src/components/login/labeledInput.tsx
+++ src/components/login/labeledInput.tsx
@@ -1,18 +1,25 @@
-import React from 'react'
-import styled from 'styled-components'
-import BigInput from '../general/bigInput'
-import Label from '../general/label'
+import React from 'react';
+import styled from 'styled-components';
+import BigInput from '../general/bigInput';
+import Label from '../general/label';
 
 const Flex = styled.div`
   display: flex;
   flex-direction: column;
-`
+`;
 
-export default function LabeledInput(props) {
+interface LabeledInputProps {
+  label: string;
+  type?: string;
+}
+
+const LabeledInput: React.FC<LabeledInputProps> = props => {
   return (
     <Flex>
       <Label>{props.label}</Label>
-      <BigInput type={props.type}/>
+      <BigInput type={props.type} />
     </Flex>
-  )
-}
+  );
+};
+
+export default LabeledInput;
--- src/components/login/loginCard.tsx
+++ src/components/login/loginCard.tsx
@@ -1,24 +1,28 @@
-import React from 'react'
-import styled from 'styled-components'
-import LabeledInput from './labeledInput'
-import Card from '../general/card'
-import Button from '../general/button'
-import H1 from '../general/H1'
+import React from 'react';
+import styled from 'styled-components';
+import LabeledInput from './labeledInput';
+import Card from '../general/card';
+import Button from '../general/button';
+import H1 from '../general/H1';
 
 const Innercard = styled.div`
-display: flex;
-flex-direction: column;
-padding: 20px 40px;
-`
+  display: flex;
+  flex-direction: column;
+  padding: 20px 40px;
+`;
 
 const CenteringContainer = styled.div`
-display:flex;
-flex-direction:row;
-margin: 10px 0px;
-justify-content:center;
-`
+  display: flex;
+  flex-direction: row;
+  margin: 10px 0px;
+  justify-content: center;
+`;
 
-export default function LoginCard(props) {
+interface LoginCardProps {
+  onConfirm: () => void;
+}
+
+const LoginCard: React.FC<LoginCardProps> = props => {
   return (
     <Card>
       <Innercard>
@@ -26,11 +30,13 @@ export default function LoginCard(props) {
           <H1>CoVoD</H1>
         </CenteringContainer>
         <LabeledInput label="Email" type="email" />
-        <LabeledInput label="Password" type="password"/>
+        <LabeledInput label="Password" type="password" />
         <CenteringContainer>
           <Button onClick={props.onConfirm}>Sign in!</Button>
         </CenteringContainer>
       </Innercard>
     </Card>
-  )
-}
+  );
+};
+
+export default LoginCard;
