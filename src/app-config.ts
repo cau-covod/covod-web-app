39656381d6d34d5338c3f101e7a245eced6e2211
Christoph Fricke <christoph@frickeonline.de>
2020-03-21 20:38:38 +0100

    Add client secret

--- src/app-config.ts
+++ src/app-config.ts
@@ -1,17 +1,20 @@
 interface Config {
   baseApiUrl: string;
   clientId: string;
+  clientSecret: string;
 }
 
 export function getConfig(): Config {
   const baseApiUrl = process.env.REACT_APP_API_BASE_URL;
   const clientId = process.env.REACT_APP_CLIENT_ID;
+  const clientSecret = process.env.REACT_APP_CLIENT_SECRET;
 
-  if (!baseApiUrl || !clientId)
+  if (!baseApiUrl || !clientId || !clientSecret)
     throw new Error('Environment config is insufficient.');
 
   return {
     baseApiUrl,
-    clientId
+    clientId,
+    clientSecret
   };
 }
