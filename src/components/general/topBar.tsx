803723749726da743675112d9bf238b67f1bdf7f
Christoph Fricke <christoph@frickeonline.de>
2020-03-21 19:22:00 +0100

    Build App together and fix errors

--- src/components/general/topBar.tsx
+++ src/components/general/topBar.tsx
@@ -1,32 +1,43 @@
-import styled from 'styled-components';
 import React from 'react';
-import Card from './card'
-import H1 from './H1'
-import { findByLabelText } from '@testing-library/react';
+import styled from 'styled-components';
+import Card from './card';
+import H1 from './H1';
+import logo from '../../sourceimages/covodLogo.png';
 
 const Flex = styled.div`
   display: flex;
   flex-direction: row;
   align-content: center;
-`
+`;
 
-export default function TopBar(props) {
+interface TopbarProps {
+  location: string;
+  user: string;
+}
+
+const Topbar: React.FC<TopbarProps> = props => {
   return (
     <Card>
-      <div style={{ display: "flex", flexDirection: "row", justifyContent: "space-between" }}>
+      <div
+        style={{
+          display: 'flex',
+          flexDirection: 'row',
+          justifyContent: 'space-between'
+        }}
+      >
         <Flex>
-          <img src={require("../../sourceimages/covodLogo.png")} width="50" height="50" />
+          <img src={logo} width="50" height="50" alt="Logo" />
           <H1>CoVoD</H1>
         </Flex>
         <Flex>
-          <p>
-            {props.location}
-          </p>
+          <p>{props.location}</p>
         </Flex>
         <Flex>
           <p>{props.user}</p>
         </Flex>
       </div>
     </Card>
-  )
-}
+  );
+};
+
+export default Topbar;
---
+++ src/screen/FeedScreen.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const FeedScreen: React.FC = () => {
+  return <h1>Feed Screen</h1>;
+};
+
+export default FeedScreen;
