6682d42452625bcfb18b9588966db3257eb38060
finite <undifini@gmail.com>
2020-03-21 18:59:55 +0100

    fix text components

--- src/components/general/H1.tsx
+++ src/components/general/H1.tsx
@@ -2,7 +2,7 @@ import styled from 'styled-components';
 
 // TODO: why is this not applied in LoginCard?
 const H1 = styled.h1`
-ont-family: 'Roboto', sans-serif;
+font-family: 'Roboto', sans-serif;
   font-size: 24px;
   font-weight: 700;
   color: ${({ theme }) => theme.colors.primary};
--- src/components/general/label.tsx
+++ src/components/general/label.tsx
@@ -1,7 +1,7 @@
 import styled from 'styled-components';
 
 const Label = styled.label`
-ont-family: 'Roboto', sans-serif;
+font-family: 'Roboto', sans-serif;
   font-size: 18px;
   font-weight: 500;
   padding: 8px 8px 4px 4px;
