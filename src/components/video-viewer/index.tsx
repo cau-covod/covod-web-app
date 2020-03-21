5587aff0befba95eaee0b2f951f938661d5a438f
Christoph Fricke <christoph@frickeonline.de>
2020-03-21 20:46:28 +0100

    Add client secret to api config

---
+++ src/components/video-viewer/index.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import VideoViewer from './video-viewer';
+
+const SmartVideoViewer: React.FC = props => {
+  return <VideoViewer />;
+};
+
+export default SmartVideoViewer;
+
+function useSlides(videoId: string) {}
--- src/services/covod-api/config.ts
+++ src/services/covod-api/config.ts
@@ -2,15 +2,21 @@ class ApiConfig {
   private isInitiated: boolean = false;
   private _baseUrl: string | undefined;
   private _clientId: string | undefined;
+  private _clientSecret: string | undefined;
   private _token: string | undefined;
 
-  public init(options: { baseUrl: string; clientId: string }) {
+  public init(options: {
+    baseUrl: string;
+    clientId: string;
+    clientSecret: string;
+  }) {
     if (this.isInitiated)
       throw new Error('CovodAPI can only be initiated once.');
 
     this.isInitiated = true;
     this._baseUrl = options.baseUrl;
     this._clientId = options.clientId;
+    this._clientSecret = options.clientSecret;
   }
 
   public get baseUrl(): string {
@@ -25,6 +31,13 @@ class ApiConfig {
       throw new Error('CovodAPI need to be initiated first.');
 
     return this._clientId;
+  }
+
+  public get clientSecret(): string {
+    if (!this.isInitiated || !this._clientSecret)
+      throw new Error('CovodAPI need to be initiated first.');
+
+    return this._clientSecret;
   }
 
   public get token(): string {
