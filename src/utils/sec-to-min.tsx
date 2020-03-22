7a9fbfa096a396d188129de3b720b8816f88ebf6
finite <undifini@gmail.com>
2020-03-22 19:09:36 +0100

    add zeroPadding

--- src/utils/sec-to-min.tsx
+++ src/utils/sec-to-min.tsx
@@ -1,6 +1,9 @@
 export function getTimeString(timestamp: number) {
-  const min = Math.floor(timestamp / 60);
-  const sec = timestamp % 60;
+  const zeroPad = (num:number, places:number) => String(num).padStart(places, '0')
+
+  const min = zeroPad(Math.floor(timestamp / 60), 2);
+  const sec = zeroPad(timestamp % 60, 2);
+
 
   return `${min}:${sec}`;
 }
