324d9ed4e84317e229bf9be03d41e6fe060d8c17
finite <undifini@gmail.com>
2020-03-22 08:52:20 +0100

    WIP FeedScreen

--- src/screen/FeedScreen.tsx
+++ src/screen/FeedScreen.tsx
@@ -1,7 +1,32 @@
 import React from 'react';
+import TopBar from '../components/general/topBar'
+import styled from 'styled-components';
+import FeedEntry from '../components/feed/feedEntry'
+
+const Background = styled.div`
+background-color: ${({ theme }) => theme.colors.primary[300]};
+display: flex;
+flex-direction:column;
+width:  100%;
+height: 100%;
+`
+
+const FeedContainer = styled.div`
+background-color: ${({ theme }) => theme.colors.grays[200]};
+width:60%;
+`
+
+
 
 const FeedScreen: React.FC = () => {
-  return <h1>Feed Screen</h1>;
+  return (
+    <Background>
+      <TopBar user="username" location="home" />
+      <FeedContainer>
+        <FeedEntries/>
+      </FeedContainer>
+    </Background>
+  )
 };
 
 export default FeedScreen;
