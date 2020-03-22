ed5d3a35204e345ef4877394c342e6b84ec0d20d
Christoph Fricke <christoph@frickeonline.de>
2020-03-22 11:57:39 +0100

    Fix login card is not centered

--- src/screen/LoginScreen.tsx
+++ src/screen/LoginScreen.tsx
@@ -1,33 +1,36 @@
 import React, { useEffect } from 'react';
 import { useAuth } from '../provider/authentication-provider';
-import LoginCard from '../components/login/loginCard'
-import styled from 'styled-components'
+import LoginCard from '../components/login/loginCard';
+import styled from 'styled-components';
 
 const LoginScreen: React.FC = () => {
   const { login } = useAuth();
 
   const CenterFlex = styled.div`
-display:flex;
-flex-direction:column;
-justify-content: center;
-align-items:center;
-`
+    height: 100vh;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+  `;
 
-const Flex = styled.div`
-  display: flex;
-  flex-direction: row;
-  align-content: center;
-`;
+  const Flex = styled.div`
+    display: flex;
+    flex-direction: row;
+    align-content: center;
+  `;
 
   return (
     <CenterFlex>
-      <div style={{height: "200px" }}/>
-      <div style={{ width: "60%", height: "60%" }}>
-        <LoginCard onConfirm={(username:string, password:string) => login(username, password)} />
+      <div style={{ width: '60%', height: '60%' }}>
+        <LoginCard
+          onConfirm={(username: string, password: string) =>
+            login(username, password)
+          }
+        />
       </div>
     </CenterFlex>
-  )
-
+  );
 };
 
 export default LoginScreen;
