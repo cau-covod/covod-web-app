719016ba933c2d32aec12b8183f6af7570b84887
Christoph Fricke <christoph@frickeonline.de>
2020-03-22 18:43:40 +0100

    fix comment story

--- src/components/video-viewer/comments.stories.tsx
+++ src/components/video-viewer/comments.stories.tsx
@@ -1,42 +1,14 @@
 import React from 'react';
-import faker from 'faker';
 import Comments from './comments';
-import { id } from '../../test-utils/builders/common';
+import { commentBuilder } from '../../test-utils/builders/comment-builder';
+import { buildList } from '../../test-utils/builders/list-builder';
 
 export default {
   title: 'VideoViewer/Comments',
   component: Comments
 };
 
+const x = buildList(commentBuilder);
 export const Default = () => {
-  var x = [
-    {
-      id: id(),
-      timestamp: 124,
-      authorId: id(),
-      authorName: `${faker.name.firstName()} ${faker.name.lastName()}`,
-      content: "I don't understand",
-      replies: [
-        {
-          id: id(),
-          timestamp: 124,
-          authorId: id(),
-          authorName: `${faker.name.firstName()} ${faker.name.lastName()}`,
-          content: 'It means that x is 5/4',
-          replies: [
-            {
-              id: id(),
-              timestamp: 124,
-              authorId: id(),
-              authorName: `${faker.name.firstName()} ${faker.name.lastName()}`,
-              content: 'Ahhh, this app is really useful',
-              replies: []
-            }
-          ]
-        }
-      ]
-    }
-  ];
-  console.log(x);
   return <Comments comments={x} />;
 };
