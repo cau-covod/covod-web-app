4b1abc445842e434c58f72355b9a8e0cbdef2454
finite <undifini@gmail.com>
2020-03-27 22:14:27 +0100

    Refactor Comments Section into video viewer. Next commit, video viewer state will be handled here to allow comment section to effect state.

--- src/components/video-viewer/index.tsx
+++ src/components/video-viewer/index.tsx
@@ -1,7 +1,14 @@
 import React from 'react';
-import VideoViewer from './video-viewer';
+import styled from 'styled-components';
+
 import { useSlides } from '../../provider/slide-provider';
 import { useVideo } from '../../provider/video-provider';
+import { useLectureComments } from '../../provider/course-provider';
+
+import Card from '../general/card'
+import VideoViewer from './video-viewer';
+import ScrollingCommentSection from './comments'
+
 
 interface SmartVideoViewerProps {
   videoId: string;
@@ -10,8 +17,25 @@ interface SmartVideoViewerProps {
 const SmartVideoViewer: React.FC<SmartVideoViewerProps> = props => {
   const slides = useSlides(props.videoId);
   const vidUrl = useVideo(props.videoId);
+  const comments = useLectureComments(props.videoId);
 
-  return <VideoViewer slides={slides} videoUrl={vidUrl} />;
+  return (
+    <RowFlex>
+      <div style={{ height: '100%' }}>
+        <VideoViewer slides={slides} videoUrl={vidUrl} />
+      </div>
+      <Card style={{ minWidth: '300px' }}>
+        <ScrollingCommentSection comments={comments} />
+      </Card>
+    </RowFlex>
+  )
 };
+
+const RowFlex = styled.div`
+  display: flex;
+  direction: row;
+  justify-content: center;
+  max-height: 75vh;
+`;
 
 export default SmartVideoViewer;
